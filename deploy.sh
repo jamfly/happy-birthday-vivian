db55ee4164a25a147958204038544d7aee73666d
jamfly <cocoredju222@gmail.com>
2019-10-07 10:02:55 +0800

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f https://github.com/DevilTea/happy-birthday-yaoandy.git master:gh-pages
+git push -f https://github.com/jamfly/happy-birthday-yaoandy.git master:gh-pages
 
 cd -
